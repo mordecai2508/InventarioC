b2e4f2780bd83050cf60313abf5ce9879e157b0b
Mordecari <rcarlosa2004@gmail.com>
2025-04-09 16:33:41 -0500

    Conexion con la BD - Metodo HTTP:GET

--- Backend/src/controllers/categorias.controller.js
+++ Backend/src/controllers/categorias.controller.js
@@ -1,5 +1,12 @@
-const getCategorias = (req,res)=>{
-    res.json({"Categoria":"Electrodomesticos"})
+import getConenection from "../db/database.js"
+const getCategorias = async (req,res)=>{
+    try {
+        const connection = await getConenection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias")
+        res.json(result)
+    } catch (error) {
+        console.error("ERROR 500, no conecta con la base de datos :(");
+    }
 }
 
 export const methodHTTP = {
